c86d3f713a8de4f434957f1a62698053b7cd498c
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-10 23:34:09 +0100

    Created the specialbutton and operator button component

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const OperatorButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
-};
+// const OperatorButton = () => {
+//   return (
+//     <>
+//       {/* Display a button element rendering the data being passed down from the parent container on props */}
+//     </>
+//   );
+// };
+
+function OperatorButton(props) {
+  const { char, value } = props;
+  return <button className="operatorButton">{char}</button>;
+}
+
+export default OperatorButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const SpecialButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
-};
+// const SpecialButton = (props) => {
+// const { specialCharacters } = props;
+//   return (
+//     <>
+//       {/* Display a button element rendering the data being passed down from the parent container on props */}
+//     </>
+//   );
+// };
+
+function SpecialButton(props) {
+  const { specialCharacters } = props;
+  return <button className="specialButton">{specialCharacters}</button>;
+}
+
+export default SpecialButton;
