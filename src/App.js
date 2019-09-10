e6a0cc43885f252a1a790da68365c5227d230bb7
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-11 00:47:40 +0100

    MVP completed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
+  // Don't forget to pass the functions (and any additional data needed) to the components  props
 
   const [display, setDisplay] = useState(0);
 
