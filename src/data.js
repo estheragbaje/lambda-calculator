57f50d912616749d439e35499ba18dd229f1e7f6
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-11 12:44:49 +0100

    Added comments to each step I carried out on data.js

--- src/data.js
+++ src/data.js
@@ -30,3 +30,24 @@ export const operators = [
 ];
 
 export const specials = ["C", "+/-", "%"];
+
+/**
+ * STEPS I FOLLOWED
+ * 1.. Exported Numbers, Operators and Specials
+ * 2.. Imported them into their individual js files for Numbers, Operators and Specials e.g import { numbers } from "../../../data";
+ * 3.. Also made sure I was exporting the necessasry files
+ * 4.. Destructured the onClick props e.g
+ *    const { digit, onClick } = props;
+ * 5.. Created a function handleclick that takes in the callback e.g const handleClick = () => onClick(digit);
+ * 6.. Performed a map on all the numbers, operators and special to produce all the buttons to create the components of the calculator
+ * 7.. Created the display component and exported it
+ * 8.. Imported numbers, operators, specials and display into app.js e.g import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+ * 9.. Styled the buttons usings CSS and added classNames where necessary to make styling easier
+ * 10.. In Apps.js Set useState to 0 and passed two arguments{display, SetDisplay}
+ * 11.. Created funtions to handle the logic for numbers, operators and special buttons
+ * 12.. For Numbers, the output should be equal to the display and if display is 0, it shouldn't display anyother thing, if not, it should display 0
+ *      For Operators, if display is 0, no operaor button should show. If "=" is used, it should evaluate mathematically what has been passed into it and bring out the output
+ * 13.. For SpecialClick, if C is clicked, it should set display to 0, % should divide by 100 and +- should multiply display by -1
+ * 14.. I then rendered all (display, special, numbers, operators).
+ * 15.. I had to create a div on special and numbers called special-numbers and then a div called all-buttons containing special-numbers div and operators and styled in css till I achieved the result
+ */
